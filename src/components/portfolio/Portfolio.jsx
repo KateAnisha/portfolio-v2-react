0b7f478e630f324226c5983462a7a9918d35ba0f
kate.anisha <kate.anisha@icloud.com>
2021-06-12 12:08:32 +1000

    trying deploy again

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,21 +5,21 @@ export default function Portfolio() {
         <div className="portfolio" id="portfolio">
             <h2>Portfolio</h2>
             <div className="portfolio-container">
-                <a href="https://quiet-island-78989.herokuapp.com/" target="_blank">
+                <a href="https://quiet-island-78989.herokuapp.com/" target="_blank" rel="noreferrer">
                     <div className="portfolio-item">
                         <h3>Slayerz</h3>
                         <img src="assets/slayerz.png" alt="portfolio" />
                     </div>
                 </a>
 
-                <a href="https://rogerferrier.legal" target="_blank">
+                <a href="https://rogerferrier.legal" target="_blank" rel="noreferrer">
                     <div className="portfolio-item">
                         <h3>Roger Ferrier Law</h3>
                         <img src="assets/rfl.png" alt="portfolio" />
                     </div>
                 </a>
 
-                <a href="https://radiancebeautybar.com.au/" target="_blank">
+                <a href="https://radiancebeautybar.com.au/" target="_blank" rel="noreferrer">
                     <div className="portfolio-item">
                         <h3>Radiance Beauty Bar</h3>
                             <img src="assets/rbb.png" alt="Radiance Beauty Bar" />
